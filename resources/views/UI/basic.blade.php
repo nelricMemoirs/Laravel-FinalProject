ed98353a7ec1952be1d9cf59a897c721de8122bd
Arief Luqman <bookswithlov3@gmail.com>
2020-07-09 18:18:27 +0700

    edit weclome dan add folder UI

---
+++ resources/views/UI/basic.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app') {{-- merupakan navbar/ menu bar atas  --}}
+
+@section('content')  {{-- section content dari layouts.app  --}}
+    
+    {{-- isi kodingan  --}}
+
+@endsection
